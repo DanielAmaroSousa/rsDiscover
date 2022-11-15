d84391c671f92a0f3e07ad9455de1777a8ee2fa7
Daniel Amaro Sousa <dsousapt@gmail.com>
2022-11-15 08:46:17 +0000

    Manipulação de dados e Operadores

--- lesson16_manipdados.js
+++ lesson16_manipdados.js
@@ -14,11 +14,66 @@ console.log(Number('9')+5) //conversão =14
 //conversão e manipulação de números e strings
 
 let string = "123"
-conslole.log(Number(string))
+console.log(Number(string))
 
 let number = 55
 console.log(String(number))
 
 let number2 = 134509861.98696195
-console.log(Number(number2.toFixed(2).replace(".",",")))
+console.log(number2.toFixed(2).replace(".",","))
 
+//uppercase  e lowercase
+let word = "Programar é muito bacana!"
+console.log(word.toLowerCase())
+
+//texto para array
+let texto = "Programar é fixe pois é?"
+let myArray = texto.split(" ")
+console.log(myArray)
+let frase = myArray.join("_")
+console.log(frase)
+
+//verificar se a frase contém a palavra Amor
+
+let frase2 = "eu procuro o amor"
+console.log(frase2.includes("Amor"))
+console.log(frase2.includes("amor"))
+
+//criar array com construtor
+
+let myArray2 = new Array(10)
+let myArray3 = new Array(1, 2, 3)
+console.log(myArray2)
+console.log(myArray3)
+
+//contar elementos de um array
+
+console.log(["a","b","c"].length)
+
+//transformar uma cadeia de caracteres em elementos 
+//de um array
+
+let word2 = "manipulação"
+console.log(Array.from(word2))
+
+//manipulando arrays
+
+let techs = ["html", "css", "js"]
+//adicionar no fim
+techs.push("nodejs")
+//adicionar no inicio
+techs.unshift("sql")
+//remover do fim
+//techs.pop()
+//remover do inicio
+//techs.shift()
+//pegar somente alguns elementos
+console.log(techs.slice(0,2))
+//remover 1 ou mais items do array
+//techs.splice(1, 2)
+//encontrar a posição do elemento no array
+let index = techs.indexOf("css")
+console.log(index)
+console.log(techs)
+techs.splice(index,1)
+console.log(techs)
---
+++ lesson17_expressoes_operadores.js
@@ -0,0 +1,9 @@
+//expressões e operadores
+
+let number = 1
+//operador binário
+console.log(number + 1)
+//operador unário (++, --)
+console.log(++number)
+//operador ternário
+console.log(false ? 'alo' : 'nada')
